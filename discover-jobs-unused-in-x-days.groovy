d68450a8e5e5a69200e54f4adde652e584d473cd
tsingkofer-cb <92389252+tsingkofer-cb@users.noreply.github.com>
2022-07-22 09:18:44 -0500

    Create discover-jobs-unused-in-x-days.groovy

---
+++ discover-jobs-unused-in-x-days.groovy
@@ -0,0 +1,19 @@
+import hudson.model.Job
+import jenkins.model.Jenkins
+
+def days = 365
+def cutOffTime = System.currentTimeMillis() - 1000L * 60 * 60 * 24 * days
+def cutOffDate = new Date(cutOffTime).format("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+
+println "Finding jobs that have not been built since ${cutOffDate} (more than ${days} days ago)..."
+for (job in Jenkins.instance.getAllItems(Job.class)) {
+
+    def build = job.getLastSuccessfulBuild()
+
+    if (build != null && build.getTimeInMillis() < cutOffTime) {
+        println ""
+        println "Job Name:     " + job.fullName
+        println "Build Number: " + build.number
+        println "Job Last Run: " + build.timestampString2
+    }
+}
