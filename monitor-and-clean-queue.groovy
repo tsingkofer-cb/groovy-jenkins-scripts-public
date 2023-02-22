55ab39600853c39ad642119e6ae1f5185de5909b
tsingkofer-cb <92389252+tsingkofer-cb@users.noreply.github.com>
2023-02-22 12:55:05 -0600

    add url to build

--- monitor-and-clean-queue.groovy
+++ monitor-and-clean-queue.groovy
@@ -4,13 +4,15 @@ def currentTime = new Date().getTime()
 def timeLimitToAbort = 60000 * 20 //20 minutes)
 
 Jenkins.instance.queue.items.each {
-    println "Queue ID: $it.id, Assigned Label: $it.assignedLabel, Time in Queue: $it.inQueueForString, Cause: $it.causeOfBlockage"
+    println "URL: $it.task.url\n Queue ID: $it.id\n Assigned Label: $it.assignedLabel\n Time in Queue: $it.inQueueForString\n Cause: $it.causeOfBlockage"
     waitingTime = currentTime-it.getInQueueSince()
   if (waitingTime > timeLimitToAbort) {
     println 'Aborting Build since it has been stuck in the queue for more than the allowed time'
     Jenkins.instance.queue.cancel(it)
+    println '---------------------------'
   } else {
     println 'Build has been waiting for ' +  waitingTime/1000/60 + ' minutes. Will not abort.'
+    println '---------------------------'
   }
 }
 
