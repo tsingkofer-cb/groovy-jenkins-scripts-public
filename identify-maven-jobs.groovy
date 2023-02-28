66f63d5ef33c4728e9bd0771cfce786d45062043
tsingkofer-cb <92389252+tsingkofer-cb@users.noreply.github.com>
2023-02-28 10:40:38 -0600

    Update identify-maven-jobs.groovy

--- identify-maven-jobs.groovy
+++ identify-maven-jobs.groovy
@@ -1,8 +1,8 @@
-println 'Listing all Maven Integration Plugin jobs on this instance...'
-Jenkins.instance.getAllItems(Job).each{job -> job.isBuildable()
-
-if (job.isBuildable()!=null && ( job.class.toString() == 'class hudson.maven.MavenModuleSet' || job.class.toString() == 'class hudson.maven.MavenModule' ) )
-	println job.fullName
+def plugin = Jenkins.get().getPlugin('maven-plugin')
+if (plugin != null) {
+  println 'maven-plugin installed, used by the following jobs:'
+  Jenkins.get().getAllItems(hudson.maven.AbstractMavenProject.class).each { println it.fullName }
+} else {
+  println 'maven-plugin not installed'
 }
-
 return null
