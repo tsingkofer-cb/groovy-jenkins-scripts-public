4d9a27cfdf937ed02d38d89b3f201e9ecf6614eb
tsingkofer-cb <92389252+tsingkofer-cb@users.noreply.github.com>
2023-03-01 08:21:46 -0600

    show last build timestamp if it exists

--- identify-maven-jobs.groovy
+++ identify-maven-jobs.groovy
@@ -1,7 +1,14 @@
 def plugin = Jenkins.get().getPlugin('maven-plugin')
 if (plugin != null) {
   println 'maven-plugin installed, used by the following jobs:'
-  Jenkins.get().getAllItems(hudson.maven.AbstractMavenProject.class).each { println it.fullName }
+  Jenkins.get().getAllItems(hudson.maven.AbstractMavenProject.class).each { 
+    def lastBuild = it.getLastBuild()
+    if (lastBuild) {
+	    println it.fullName + ' - last built on: ' + it.getLastBuild().getTimestampString2() 
+    } else {
+      println it.fullName + ' - contains no build records, likely was never run.'
+    }
+  }
 } else {
   println 'maven-plugin not installed'
 }
