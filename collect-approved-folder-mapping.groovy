e1bc797cfca7809dd470a7e994ffc6ee40e620a1
tsingkofer-cb <92389252+tsingkofer-cb@users.noreply.github.com>
2023-10-04 15:57:00 -0500

    Update collect-approved-folder-mapping.groovy

--- collect-approved-folder-mapping.groovy
+++ collect-approved-folder-mapping.groovy
@@ -1,24 +1,27 @@
 import com.cloudbees.hudson.plugins.folder.Folder
 import com.cloudbees.jenkins.plugins.foldersplus.SecurityGrantsFolderProperty;
 
+def outputCsv = true //set to false for more readable script output, true for csv format
+
 def folderItems = Jenkins.instance.getAllItems(Folder.class)
 
 //CSV Column Headings
-println "Agent,Folder"
-
+if (outputCsv){
+  println "Agent,Folder"
+}
 for (folderItem in folderItems) {
-	//println "Folder : " + folderItem.name
-
   SecurityGrantsFolderProperty securityProperty = SecurityGrantsFolderProperty.of(folderItem)
   if (securityProperty != null) {
     securityProperty.getSecurityGrants().each {
       //For more readable script output instead of csv
-      //println "  SecurityGrant : "
-      //println "    Folder: " + it.getFolder().name
-      //println "    Node: " + it.getNode().name
-      
-      //For CSV format:
-      println it.getNode().name + ',' + it.getFolder().name
+      if(!outputCsv){
+        println "  SecurityGrant : "
+        println "    Folder: " + it.getFolder().fullName
+        println "    Node: " + it.getNode().name
+      }
+      if(outputCsv){
+        println it.getNode().name + ',' + it.getFolder().fullName
+      }
     }
   }
 }
