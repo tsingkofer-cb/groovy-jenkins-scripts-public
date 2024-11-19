faf53641f059e620f2801b9bb080e9841a862bab
tsingkofer-cb <92389252+tsingkofer-cb@users.noreply.github.com>
2024-11-19 15:59:37 -0600

    Update identify-broken-agents.groovy

--- identify-broken-agents.groovy
+++ identify-broken-agents.groovy
@@ -1,15 +1,15 @@
 for (agent in hudson.model.Hudson.instance.slaves) {
-  if(agent.getComputer()){
-    comp = agent.getComputer()
-    if(comp.isOffline()){ //ignore any agents that are already online, we know those are fine.
-	    comp.connect(false)
-    	sleep 5000
-      if(!comp.isOnline()){ //if the agent is still offline after attempting to connect, print the display name and the agent log.
-        println '*** ' + comp.getDisplayName() + ' ***'
-        println comp.getLog()
-        println comp.getOfflineCauseReason()
-      }
-      comp.disconnect() //disconnect the agent again to finish up
-    }
-  }
+	if(agent.getComputer()){
+    	comp = agent.getComputer()
+    	if(comp.isOffline()){ //ignore any agents that are already online, we know those are fine.
+			comp.connect(false)
+    		sleep 5000
+      		if(!comp.isOnline()){ //if the agent is still offline after attempting to connect, print the display name and the agent log.
+        		println '*** ' + comp.getDisplayName() + ' ***'
+        		println comp.getLog()
+        		println comp.getOfflineCauseReason()
+      		}
+      		comp.disconnect() //disconnect the agent again to finish up
+    	}
+  	}
 }
