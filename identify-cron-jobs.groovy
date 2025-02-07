884017a41d008385adfe9c87083f50add115f8f3
tsingkofer-cb <92389252+tsingkofer-cb@users.noreply.github.com>
2025-02-07 12:41:18 -0600

    Update identify-cron-jobs.groovy

--- identify-cron-jobs.groovy
+++ identify-cron-jobs.groovy
@@ -1,12 +1,24 @@
-println 'Listing all jobs with a cron timer set... \n'
+import hudson.triggers.TimerTrigger
+import com.cloudbees.hudson.plugins.folder.computed.PeriodicFolderTrigger 
 
-Jenkins.instance.getAllItems(Job.class).each{
-  if (it.triggers.values()) {  
-    println it.fullName
-    for (trigger in it.triggers.values()) {
-      println trigger.spec 
+println 'Listing all items with a timer set...'
+
+Jenkins.instance.getAllItems().each{
+  if(it.hasProperty('triggers')){
+    if (it.triggers.values()) { 
+      timerExists = it.triggers.values().any{ t -> t.getClass() == TimerTrigger || t.getClass() == PeriodicFolderTrigger }
+      if (timerExists){
+      	println ''
+        println it.absoluteUrl
+        println it.getClass()
+      	for (trigger in it.triggers.values()) {
+        	println trigger.spec
+        	if (trigger.hasProperty('interval')){
+	        	println trigger.interval 
+        	}
+      	}
+      }
     }
-    println ''
   }
 }
   
