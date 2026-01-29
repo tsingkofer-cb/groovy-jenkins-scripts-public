426e4cede7f1ec58a41fa079a35dad586c14813e
tsingkofer-cb <92389252+tsingkofer-cb@users.noreply.github.com>
2026-01-29 16:43:51 -0600

    Create disable-org-child-scan-trigger.groovy

---
+++ disable-org-child-scan-trigger.groovy
@@ -0,0 +1,25 @@
+import jenkins.branch.OrganizationChildTriggersProperty
+
+dryRun = true
+
+println 'Listing all org jobs with timer set'
+if (!dryRun){
+  println 'Will also disable cron setting \n'
+}
+
+Jenkins.instance.getAllItems(jenkins.branch.OrganizationFolder.class).each {
+  if (it.properties.any{ t -> t.getClass() == OrganizationChildTriggersProperty }){
+  	println it.fullName + " has a child timer"
+    if (!dryRun){
+      for (prop in it.properties) {
+        if (prop.getClass() == OrganizationChildTriggersProperty){
+			println "Removing Child Scan Trigger"
+            it.properties.remove(prop)
+          	it.save()
+        }
+      }
+    }
+  }
+}
+  
+null
